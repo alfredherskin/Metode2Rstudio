f4936aad09b8540a868f5cdf59bfc2ec6ecc03e8
alfredherskin <alfred.herskin@gmail.com>
2024-11-12 14:20:45 +0100

    Add files via upload

---
+++ metode2.R
@@ -0,0 +1,9 @@
+getwd()
+setwd("C:/Users/alfre/OneDrive/Desktop/KU/Statskundskab/3. semester/Metode 2/eksamen/R")
+library(haven)
+library(tidyverse)
+library(stargazer)
+
+data <- read.csv("ESS7e02_3.csv")
+
+write_dta(data, "ESS7e02_3.dta")
